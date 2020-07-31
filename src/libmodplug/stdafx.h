d77d81920fa12594a3b223292ba400bf55d7a1d1
sezero <sezero@users.noreply.github.com>
2020-07-31 23:33:00 +0300

    stdafx.h: remove unused MulDiv inline

--- src/libmodplug/stdafx.h
+++ src/libmodplug/stdafx.h
@@ -88,12 +88,6 @@ typedef const char* LPCSTR;
 typedef void* PVOID;
 typedef void VOID;
 
-inline LONG MulDiv (long a, long b, long c)
-{
-/*if (!c) return 0;*/
-  return ((uint64_t) a * (uint64_t) b ) / c;
-}
-
 #define LPCTSTR LPCSTR
 #define lstrcpyn strncpy
 #define lstrcpy strcpy
